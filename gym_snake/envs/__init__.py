6949baaaf099f47aa9e2d9cf9b8bf3afce3f6d4a
simonandras <simonandras97@gmail.com>
2020-10-15 15:55:01 +0200

    debug foo 3

--- gym_snake/envs/__init__.py
+++ gym_snake/envs/__init__.py
@@ -1,4 +1,4 @@
 
-from gym_foo.envs.snake_env import SnakeEnv
-from gym_foo.envs.snake_extrahard_env import SnakeExtraHardEnv
+from gym_snake.envs.snake_env import SnakeEnv
+from gym_snake.envs.snake_extrahard_env import SnakeExtraHardEnv
 
